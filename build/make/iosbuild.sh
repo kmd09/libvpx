8e8f09a82e3b749c920d4a3f03c53a6af13d05e9
Brion Vibber <bvibber@wikimedia.org>
2016-05-02 12:41:59 -0400

    Add --enable-shared option to iosbuild.sh to build dynamic framework
    Enabling the shared framework bumps the iOS deployment target to 8.0,
    the minimum required to support dynamic framework deployment in apps.
    
    When not using --enable-shared, a static library for iOS 6.0+ will still
    be built.
    
    Minimum version settings have been moved into ios-version.sh so they
    can be updated in a single place.
    
    BUG=https://bugs.chromium.org/p/webm/issues/detail?id=1092
    
    Change-Id: I28fb06ce65e48ed167a88c14a7bfb2861989317e

--- build/make/iosbuild.sh
+++ build/make/iosbuild.sh
@@ -44,6 +44,7 @@ build_target() {
   local target="$1"
   local old_pwd="$(pwd)"
   local target_specific_flags=""
+  local shared_flags=""
 
   vlog "***Building target: ${target}***"
 
@@ -54,9 +55,13 @@ build_target() {
       ;;
   esac
 
+  if [ "$ENABLE_SHARED" = "yes" ]; then
+    shared_flags="--enable-shared"
+  fi
+
   mkdir "${target}"
   cd "${target}"
-  eval "${LIBVPX_SOURCE_DIR}/configure" --target="${target}" \
+  eval "${LIBVPX_SOURCE_DIR}/configure" --target="${target}" ${shared_flags} \
     ${CONFIGURE_ARGS} ${EXTRA_CONFIGURE_ARGS} ${target_specific_flags} \
     ${devnull}
   export DIST_DIR
@@ -157,7 +162,12 @@ build_framework() {
   for target in ${targets}; do
     build_target "${target}"
     target_dist_dir="${BUILD_ROOT}/${target}/${DIST_DIR}"
-    lib_list="${lib_list} ${target_dist_dir}/lib/libvpx.a"
+    if [ "$ENABLE_SHARED" = "yes" ]; then
+      local suffix="dylib"
+    else
+      local suffix="a"
+    fi
+    lib_list="${lib_list} ${target_dist_dir}/lib/libvpx.$suffix"
   done
 
   cd "${ORIG_PWD}"
@@ -175,6 +185,22 @@ build_framework() {
 
   # Copy in vpx_version.h.
   cp -p "${BUILD_ROOT}/${target}/vpx_version.h" "${HEADER_DIR}"
+
+  if [ "$ENABLE_SHARED" = "yes" ]; then
+    # Adjust the dylib's name so dynamic linking in apps works as expected
+    install_name_tool -id '@rpath/VPX.framework/VPX' ${FRAMEWORK_DIR}/VPX
+
+    # Copy in Info.plist.
+    cat "${SCRIPT_DIR}/ios-Info.plist" | \
+      sed "s/\${VERSION}/${VERSION}/g" | \
+      sed "s/\${IOS_VERSION_MIN}/${IOS_VERSION_MIN}/g" \
+      > "${FRAMEWORK_DIR}/Info.plist"
+
+    # Copy in module_map
+    mkdir "${FRAMEWORK_DIR}/Modules"
+    cp -p "${SCRIPT_DIR}/ios-module.modulemap" \
+      "${FRAMEWORK_DIR}/Modules/module.modulemap"
+  fi
 
   vlog "Created fat library ${FRAMEWORK_DIR}/VPX containing:"
   for lib in ${lib_list}; do
@@ -213,6 +239,7 @@ iosbuild_usage() {
 cat << EOF
   Usage: ${0##*/} [arguments]
     --help: Display this message and exit.
+    --enable-shared: Build a dynamic framework for use on iOS 8 or later.
     --extra-configure-args <args>: Extra args to pass when configuring libvpx.
     --macosx: Uses darwin15 targets instead of iphonesimulator targets for x86
               and x86_64. Allows linking to framework when builds target MacOSX
@@ -251,6 +278,9 @@ while [ -n "$1" ]; do
       iosbuild_usage
       exit
       ;;
+    --enable-shared)
+      ENABLE_SHARED=yes
+      ;;
     --preserve-build-output)
       PRESERVE_BUILD_OUTPUT=yes
       ;;
@@ -278,6 +308,15 @@ while [ -n "$1" ]; do
   shift
 done
 
+selfpath=`dirname $0`
+VERSION=`"$selfpath"/version.sh --bare "$selfpath/../.." | sed -E 's/^v([0-9]+\.[0-9]+\.[0-9]+)(.*)$/\1/'`
+if [ "$ENABLE_SHARED" = "yes" ]; then
+  IOS_VERSION_OPTIONS="--enable-shared"
+else
+  IOS_VERSION_OPTIONS=""
+fi
+IOS_VERSION_MIN=`"$selfpath/ios-version.sh" $IOS_VERSION_OPTIONS`
+
 if [ "${VERBOSE}" = "yes" ]; then
 cat << EOF
   BUILD_ROOT=${BUILD_ROOT}
@@ -292,8 +331,11 @@ cat << EOF
   ORIG_PWD=${ORIG_PWD}
   PRESERVE_BUILD_OUTPUT=${PRESERVE_BUILD_OUTPUT}
   TARGETS="$(print_list "" ${TARGETS})"
+  ENABLE_SHARED=${ENABLE_SHARED}
   OSX_TARGETS="${OSX_TARGETS}"
   SIM_TARGETS="${SIM_TARGETS}"
+  VERSION="${VERSION}"
+  IOS_VERSION_MIN="${IOS_VERSION_MIN}"
 EOF
 fi
 
